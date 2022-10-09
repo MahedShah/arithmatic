91e5410c8c9c301fe1a5e6961091609df3c9be73
MahedShah <99177018+MahedShah@users.noreply.github.com>
2022-10-09 21:14:57 +0500

    Add files via upload

---
+++ evenodd/src/evenodd/Evenodd.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package evenodd;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author mahed
+ */
+public class Evenodd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   System.out.println("Enter any number");
+    Scanner sc=new Scanner(System.in);
+    int a;
+    a= sc.nextInt();
+  
+   if(a%2==0) 
+   System.out.println("number is odd");
+    else
+        System.out.println("number is odd");
+   
+    
+    }
+   
+    
+  
+   
+    
+ 
+}
